46e1a84a9de301bc19dbc7786f4c3039bce29316
Samuel <sksksjakskska@gmail.com>
2025-08-13 01:00:21 +0900

    connect

--- mypage/src/components/Connect.js
+++ mypage/src/components/Connect.js
@@ -9,14 +9,17 @@ function Connect() {
         <a href="https://mail.naver.com" target="_blank" rel="noopener noreferrer" className="connect-box naver">
           <i className="icon-placeholder"></i>
           <h3>Naver Mail</h3>
+          <p>greenchoiid@naver.com</p>
         </a>
         <a href="https://mail.google.com" target="_blank" rel="noopener noreferrer" className="connect-box gmail">
           <i className="icon-placeholder"></i>
           <h3>Gmail</h3>
+          <p>sksksjakskska@gmail.com</p>
         </a>
         <a href="https://www.instagram.com/samuel._.0fficial/" target="_blank" rel="noopener noreferrer" className="connect-box instagram">
           <i className="icon-placeholder"></i>
           <h3>Instagram</h3>
+          <p>@samuel._.official</p>
         </a>
       </div>
     </div>
